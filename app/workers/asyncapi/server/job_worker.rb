6ab9a0cae23dbc5c14eea0a93f5ade0a79607300
Neil Marion dela Cruz <neilmarion@aelogica.com>
2016-08-30 09:15:46 +0800

    Add comment to explain purpose of the code
    [#127624879]

--- app/workers/asyncapi/server/job_worker.rb
+++ app/workers/asyncapi/server/job_worker.rb
@@ -20,6 +20,11 @@ module Asyncapi::Server
         job.update_attributes(status: job_status)
         report_job_status(job, job_message)
       else
+        # For some reason "ActiveRecord::Base.after_transaction",
+        # ":after_commit" and ":after_create" does not prevent
+        # the ActiveRecord-Sidekiq race condition. In order to
+        # prevent this just retry running JobWorker until it finds
+        # the job by job_id.
         if retries < MAX_RETRIES
           JobWorker.perform_async(job_id, retries+1)
         end
