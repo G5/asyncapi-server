689bf7cd8554465b0fb537e1927a0590d7048b35
Gabriel Sandoval <gabriel@aelogica.com>
2018-03-22 08:15:10 +0800

    [#155929975] Add param setter to asyncapi job - params should be of type Hash on access

--- app/models/asyncapi/server/job.rb
+++ app/models/asyncapi/server/job.rb
@@ -5,6 +5,11 @@ module Asyncapi
       self.table_name = "asyncapi_server_jobs"
       enum status: %i[queued success error]
 
+      def params=(params)
+        params = JSON.parse(params) unless params.is_a? Hash
+        write_attribute :params, params
+      end
+
       def url
         Engine.routes.url_helpers.v1_job_url(self)
       end
