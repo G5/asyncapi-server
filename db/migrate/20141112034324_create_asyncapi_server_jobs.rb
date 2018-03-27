b491f7c7dd4e69ddb9199c758fbb05e84ca311be
Gabriel Sandoval <gabriel@aelogica.com>
2018-03-27 09:22:17 +0800

    [#155929975] Add Rails version 4.2 on previous migrations

--- db/migrate/20141112034324_create_asyncapi_server_jobs.rb
+++ db/migrate/20141112034324_create_asyncapi_server_jobs.rb
@@ -1,4 +1,4 @@
-class CreateAsyncapiServerJobs < ActiveRecord::Migration
+class CreateAsyncapiServerJobs < ActiveRecord::Migration[4.2]
   def change
     create_table :asyncapi_server_jobs do |t|
       t.integer :status
--- db/migrate/20150130062520_add_expired_at_to_asyncapi_server_job.rb
+++ db/migrate/20150130062520_add_expired_at_to_asyncapi_server_job.rb
@@ -1,4 +1,4 @@
-class AddExpiredAtToAsyncapiServerJob < ActiveRecord::Migration
+class AddExpiredAtToAsyncapiServerJob < ActiveRecord::Migration[4.2]
   def change
     add_column :asyncapi_server_jobs, :expired_at, :datetime
 
